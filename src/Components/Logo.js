6f717a052a259d70c76f4ec65f997a689f1986bf
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-19 18:21:13 +0530

    Use : navigation hook

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 import Logo from '../pictures/WebsiteLogo.svg'
 
 export default function Heading() {
+  const navigate = useNavigate();
+  function handleLocation(){
+    navigate(`/`)
+  }
   return (
     <div>
-      <img src={Logo} alt="" />
+      <img src={Logo} className='cursor-pointer' onClick={handleLocation} alt="" />
     </div>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,15 +2,20 @@ import React from 'react'
 import AppLogo from './Logo'
 import Search from './Search'
 import Addfunc from './Addfunc'
+import { useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  function handleLocation(){
+    navigate(`/`)
+  }
   return (
     <>
     <div className=' fixed top-0 z-40 flex justify-between px-8 py-6 items-center w-full bg-gray-100'>
       <div className='flex space-x-3'>
       <div className='flex space-x-2 justify-center items-center'>
         <AppLogo/>
-        <p className='font-bold text-[18px]'>ExploreHub</p>
+        <p className='font-bold text-[18px] cursor-pointer' onClick={handleLocation}>ExploreHub</p>
         </div> <Search/>
       </div>
       <Addfunc/>
